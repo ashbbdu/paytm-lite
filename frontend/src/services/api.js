b7019c25bff67decf00e526d724e9855084388ee
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-01-23 15:48:12 +0530

    update base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://localhost:4000/api/v1/"
+// export const BASE_URL = "http://localhost:4000/api/v1/"
+
+export const BASE_URL = "https://paytm-lite-8q0v.onrender.com/api/v1/"
